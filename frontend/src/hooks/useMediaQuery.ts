a616393f8da7fcae886e60970f98d26bec3358aa
Md Asif Akhtar <asifakhtar028@gmail.com>
2025-01-13 02:13:49 +0530

    bug fix

--- frontend/src/hooks/useMediaQuery.ts
+++ frontend/src/hooks/useMediaQuery.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const useMediaQuery = (query: string): boolean => {
+export const useMediaQuery = (query: string): boolean => {
   const [matches, setMatches] = useState<boolean>(false);
 
   useEffect(() => {
@@ -22,5 +22,3 @@ const useMediaQuery = (query: string): boolean => {
 
   return matches;
 };
-
-export default useMediaQuery;
